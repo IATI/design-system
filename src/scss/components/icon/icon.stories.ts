034007a7e7c0f4581461042cfa56b16f578500ad
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-05-29 12:24:19 +0300

    feat: add social icons

---
+++ src/scss/components/icon/icon.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+const meta: Meta = {
+  title: "Components/Icon",
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Youtube: Story = {
+  render: () => html`<i class="iati-icon iati-icon--youtube"></i>`,
+};
+
+export const Twitter: Story = {
+  render: () => html`<i class="iati-icon iati-icon--twitter"></i>`,
+};
+
+export const Linkedin: Story = {
+  render: () => html`<i class="iati-icon iati-icon--linkedin"></i>`,
+};
