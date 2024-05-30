5b1b1764c2bfc832c224634f2ddbd8068b323036
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-05-30 14:21:24 +0300

    feat: add search bar component

---
+++ src/scss/components/search-bar/search-bar.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+
+import { html } from "lit";
+
+const meta: Meta = {
+  title: "Components/Search Bar",
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Search: Story = {
+  render: () => html`
+    <div class="iati-search-bar">
+      <input type="text" placeholder="Search" />
+      <button type="submit" class="iati-button">Go</button>
+    </div>
+  `,
+};
