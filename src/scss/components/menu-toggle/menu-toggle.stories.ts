fcc7e9a0bd8ab4613748640a28656747a3062be3
James Hall <james@agile.coop>
2024-10-01 09:15:25 +0100

    Fix linting issues.

--- src/scss/components/menu-toggle/menu-toggle.stories.ts
+++ src/scss/components/menu-toggle/menu-toggle.stories.ts
@@ -15,7 +15,9 @@ type Story = StoryObj;
 
 export const Open: Story = {
   render: () => html`
-    <button class="iati-menu-toggle iati-menu-toggle--open js-iati-menu-toggle-open">
+    <button
+      class="iati-menu-toggle iati-menu-toggle--open js-iati-menu-toggle-open"
+    >
       <span class="iati-menu-toggle__label">Menu</span>
     </button>
   `,
@@ -23,7 +25,9 @@ export const Open: Story = {
 
 export const Close: Story = {
   render: () => html`
-    <button class="iati-menu-toggle iati-menu-toggle--close js-iati-menu-toggle-close">
+    <button
+      class="iati-menu-toggle iati-menu-toggle--close js-iati-menu-toggle-close"
+    >
       <span>Close</span>
     </button>
   `,
