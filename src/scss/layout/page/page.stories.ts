2efde9d56be9163f00cf7457a0f01b01126972ab
James Hall <james@agile.coop>
2024-10-02 15:13:46 +0100

    Adds breadcrumb and message components.

--- src/scss/layout/page/page.stories.ts
+++ src/scss/layout/page/page.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
 
+import { Breadcrumb } from "../../components/breadcrumb/breadcrumb.stories";
+import { Notice as MessageNotice } from "../../components/message/message.stories";
 import { Footer } from "../footer/footer.stories";
 import { WithHorizontalNav, WithMobileNav } from "../header/header.stories";
 
@@ -18,6 +20,8 @@ export const HorizontalNav: Story = {
   render: (args) => html`
     ${WithHorizontalNav.render?.call({ ...args })}
     <main class="iati-main">
+      ${Breadcrumb.render?.call({ ...args })}
+      ${MessageNotice.render?.call({ ...args })}
       <h1>Page heading</h1>
       <p>Page contents</p>
     </main>
@@ -29,6 +33,8 @@ export const MobileNav: Story = {
   render: (args) => html`
     ${WithMobileNav.render?.call({ ...args })}
     <main class="iati-main">
+      ${Breadcrumb.render?.call({ ...args })}
+      ${MessageNotice.render?.call({ ...args })}
       <h1>Page heading</h1>
       <p>Page contents</p>
     </main>
