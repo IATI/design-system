4ad0f85a74d5dc8493052b99be17451b87ab942c
James Hall <james@agile.coop>
2024-10-22 08:05:15 +0100

    feat: add new header and footer components
    BREAKING CHANGE: consumer html for `.iati-header` and `.iati-footer` components must be updated to reflect the new structure and classes, and `.iati-title-bar` component has been removed

---
+++ src/scss/components/menu-toggle/menu-toggle.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+const meta: Meta = {
+  title: "Components/Menu Toggle",
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Open: Story = {
+  render: () => html`
+    <button
+      class="iati-menu-toggle iati-menu-toggle--open js-iati-menu-toggle-open"
+    >
+      <span class="iati-menu-toggle__label">Menu</span>
+    </button>
+  `,
+};
+
+export const Close: Story = {
+  render: () => html`
+    <button
+      class="iati-menu-toggle iati-menu-toggle--close js-iati-menu-toggle-close"
+    >
+      <span>Close</span>
+    </button>
+  `,
+};
--- src/scss/layout/header/header.stories.ts
+++ src/scss/layout/header/header.stories.ts
@@ -1,30 +1,100 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import logoColourUrl from "../../../assets/svg/logo-colour.svg";
+import { CountrySwitcher } from "../../components/country-switcher/country-switcher.stories";
+import {
+  Info as InfoButton,
+  Search as SearchButton,
+} from "../../components/header-button/header-button.stories";
+import { Open as MenuToggle } from "../../components/menu-toggle/menu-toggle.stories";
+import { MobileNav } from "../../components/mobile-nav/mobile-nav.stories";
+import { Default as TitleBar } from "../../components/title-bar/title-bar.stories";
+import { ToolNav } from "../../components/tool-nav/tool-nav.stories";
+
+const generalNavItems = ["IATI home", "News", "Events", "Contacts"];
 
 const meta: Meta = {
   title: "Layout/Header",
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    open: {
+      defaultValue: true,
+      control: { type: "boolean" },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj;
 
-export const Header: Story = {
-  render: () => html`
+const logo = html`
+  <a href="https://iatistandard.org/" aria-label="Go to IATI homepage">
+    <img src="${logoColourUrl}" class="iati-header__logo" alt="" />
+  </a>
+`;
+
+export const WithHorizontalNav: Story = {
+  render: (args) => html`
     <header class="iati-header">
-      <div>
-        <a href="https://iatistandard.org/" aria-label="Go to IATI homepage">
-          <img
-            src="http://styles.iatistandard.org/assets/svg/source/logo-colour.svg"
-            class="iati-header__logo"
-            alt=""
-          />
-        </a>
+      <div class="iati-header__section iati-header__section--first">
+        <div class="iati-header__container">
+          ${logo}
+          <nav class="iati-header__general-nav">
+            <ul class="iati-piped-list">
+              ${generalNavItems.map((i) => html`<li><a href="#">${i}</a></li>`)}
+            </ul>
+          </nav>
+        </div>
       </div>
-      <ul class="iati-header__nav">
-        <li class="iati-header__nav-item"><a>Events</a></li>
-        <li class="iati-header__nav-item"><a>Contact</a></li>
-        <li class="iati-header__nav-item"><a>Search</a></li>
-      </ul>
+      <div
+        class="iati-header__section iati-header__section--last iati-brand-background"
+      >
+        <div class="iati-header__container iati-brand-background__content">
+          <div class="iati-header__actions">
+            ${CountrySwitcher.render?.call({ ...args })}
+            ${InfoButton.render?.call({ ...args })}
+            ${SearchButton.render?.call({ ...args })}
+          </div>
+          ${TitleBar.render?.call({ ...args })}
+          <div class="iati-header__nav">
+            ${ToolNav.render?.call({ ...args })}
+          </div>
+        </div>
+      </div>
     </header>
+  `,
+};
+
+export const WithMobileNav: Story = {
+  parameters: {
+    viewport: {
+      defaultviewport: "tablet",
+    },
+  },
+  render: (args, context) => html`
+    ${MobileNav.render({ ...args }, context)}
+    <header class="iati-header">
+      <div class="iati-header__section iati-header__section--first">
+        <div class="iati-header__container">
+          ${logo}
+        </div>
+      </div>
+      <div class="iati-header__section iati-header__section--last iati-brand-background">
+        <div class="iati-header__container iati-brand-background__content">
+          <div class="iati-header__actions">
+            ${CountrySwitcher.render?.call({ ...args })}
+            ${SearchButton.render?.call({ ...args })}
+            ${MenuToggle.render?.call({ ...args })}
+          </div>
+          ${TitleBar.render?.call({ ...args })}
+          <div class="iati-header__nav" hidden>
+            ${ToolNav.render?.call({ ...args })}
+          </div>
+        <div>
+      </div>
+    </header>
+    <script src="js/header.js"></script>
   `,
 };
