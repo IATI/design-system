db73de2a77a957c5b2cbaa8d732f13ce57857d09
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-10-22 15:27:56 +0300

    docs: update capitalisation on nav items

--- src/scss/components/piped-list/piped-list.stories.ts
+++ src/scss/components/piped-list/piped-list.stories.ts
@@ -2,7 +2,7 @@ import { html } from "lit";
 
 import type { Meta, StoryObj } from "@storybook/web-components";
 
-const items = ["IATI home", "News", "Events", "Contacts"];
+const items = ["IATI Home", "News", "Events", "Contacts"];
 
 const meta: Meta = {
   title: "Components/Piped List",
