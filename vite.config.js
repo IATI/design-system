fca93eb7d498272256568b61ebee5581c77b789a
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-10-24 10:02:03 +0300

    feat: build javascript using vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ export default defineConfig({
   build: {
     lib: {
       entry: {
+        iati: path.resolve(__dirname, "src/js/main.js"),
         css: path.resolve(__dirname, "src/scss/main.scss"),
       },
+      fileName: "iati",
       formats: ["es"],
     },
     rollupOptions: {
       output: {
+        entryFileNames: "js/[name].js",
         assetFileNames: (assetInfo) => {
           if (assetInfo.name == "style.css") {
             return `css/iati.css`;
